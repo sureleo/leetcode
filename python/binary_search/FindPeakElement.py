02f25937b5f13a625049bb328f9e61a12d69a21e
sureleo <surezeroleo@gmail.com>
2015-01-21 10:45:45 -0800

    amend find peak element

--- python/binary_search/FindPeakElement.py
+++ python/binary_search/FindPeakElement.py
@@ -1,17 +1,24 @@
+#improvement: avoid of using list.insert()
+#list.insert is O(n) which violate O(logn)
 class Solution:
     # @param num, a list of integer
     # @return an integer
     def findPeakElement(self, num):
         INT_MIN = -2147483648
-        num.insert(0, INT_MIN)
-        num.append(INT_MIN)
-        
+
         low = 0
-        high = len(num) - 1
+        high = len(num)
+        if low == (high - 1):
+            return low
+
         while low <= high:
             mid = (low + high) / 2
+            if (mid == len(num) - 1) and num[mid] > num[mid-1]:
+                return mid
+            if (mid == 0) and num[mid] > num[mid+1]:
+                return mid
             if num[mid] > num[mid-1] and num[mid] > num[mid+1]:
-                return mid - 1
+                return mid
             elif num[mid-1] > num[mid]:
                 high = mid - 1
             else:
@@ -19,5 +26,5 @@ class Solution:
 
 if __name__ == "__main__":
     solution = Solution()
-    A = [-2147483648]
+    A = [1, 2, 3]
     print solution.findPeakElement(A)
