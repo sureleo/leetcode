70718438917d38fdc31e8f156ae45b2c0ad56404
sureleo <surezeroleo@gmail.com>
2015-01-26 15:32:53 -0800

    Find Minimum In Rotated Array

--- c++/array/FindMinimumInRotatedArrayII.cpp
+++ c++/array/FindMinimumInRotatedArrayII.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int findMin(vector<int> &num) {
-        int low = 0;
-        int high = int(num.size()) - 1;
-        while (low < high - 1) {
-            int mid = (low + high) / 2;
-            if (num[mid] > num[high]) {
-                low = mid;
-            } else {
-                high = mid;
-            }
-        }
-        
-        return std::min(num[low], num[high]);
+        return *std::min_element(num.begin(), num.end());
     }
 };
