9f5d565e3d50baf609edac15cbe81938abf2dac9
Shuo LI <surezeroleo@gmail.com>
2017-02-06 21:42:07 -0800

    Update PopulatingNextRightPointersInEachNodeII.java

--- java/PopulatingNextRightPointersInEachNodeII.java
+++ java/PopulatingNextRightPointersInEachNodeII.java
@@ -10,37 +10,34 @@ public class Solution {
     public void connect(TreeLinkNode root) {
         if (root == null) {
             return;
+        }    
+        
+        if (root.left != null) {
+            root.left.next = root.right != null ? root.right : getRootNextFirstChild(root.next);
         }
         
-        if (root.left != null && root.right == null) {
-            TreeLinkNode tmp = root.next;
-            this.pointTo(root.left, tmp);
-        } else if (root.left == null && root.right != null) {
-            TreeLinkNode tmp = root.next;
-            this.pointTo(root.right, tmp);
-        } else if (root.left != null && root.right != null) {
-            root.left.next = root.right;
-            TreeLinkNode tmp = root.next;
-            this.pointTo(root.right, tmp);
-        } else {
-            return;
+        if (root.right != null) {
+            root.right.next = getRootNextFirstChild(root.next);
         }
         
-        this.connect(root.right);
-        this.connect(root.left);
+        connect(root.right);
+        connect(root.left);
     }
     
-    public void pointTo(TreeLinkNode from, TreeLinkNode to) {
-        while (to != null) {
-            if (to.left != null) {
-                from.next = to.left;
-                break;
+    private TreeLinkNode getRootNextFirstChild(TreeLinkNode root) {
+        TreeLinkNode p = root;
+        while (p != null) {
+            if (p.left != null) {
+                return p.left;
             }
-            if (to.right != null) {
-                from.next = to.right;
-                break;
+            
+            if (p.right != null) {
+                return p.right;
             }
-            to = to.next;
+            
+            p = p.next;
         }
+        
+        return null;
     }
 }
