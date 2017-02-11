a71736a6b26287c7b89adc0e17b4cf8f6bc183d0
Shuo LI <surezeroleo@gmail.com>
2017-02-10 17:02:18 -0800

    Update ValidateBinarySearchTree.java

--- java/ValidateBinarySearchTree.java
+++ java/ValidateBinarySearchTree.java
@@ -1,27 +1,17 @@
-/**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public boolean isValidBST(TreeNode root) {
-        //here need to use Long rather than Integer.
-        //I think the only way to avoid edge cases like {MAX_INT} to handle this in recursion
-        //which makes the code fairly ugly
-        return this.check(root, Long.MAX_VALUE, Long.MIN_VALUE);   
+        return this.validate(root, null, null);
     }
     
-    public boolean check(TreeNode root, long root_max, long root_min) {
+    private boolean validate(TreeNode root, Integer max, Integer min) {
         if (root == null) {
             return true;
         }
-        if (root.val >= root_max || root.val <= root_min) {
+        
+        if ((max != null && root.val >= max.intValue()) || (min != null && root.val <= min.intValue())) {
             return false;
         }
-        return check(root.left, root.val, root_min) && check(root.right, root_max, root.val);
-    }
+        
+        return validate(root.left, root.val, min) && validate(root.right, max, root.val);
+    } 
 }
