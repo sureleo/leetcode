a684be6b702742c68937f0e0f0ccac87702bfa11
Shuo LI <surezeroleo@gmail.com>
2018-11-18 16:12:16 -0800

    Update ImplementTrie.java

--- java/ImplementTrie.java
+++ java/ImplementTrie.java
@@ -1,75 +1,74 @@
 class TrieNode {
-    private char val;
-    public Map<Character, TrieNode> dict;
-    public boolean isLeaf;
+    char val;
+    Map<Character, TrieNode> dict;
+    boolean isLeaf;
     
-    public TrieNode (char val) {
-        this.val = val;
-        this.dict = new HashMap<Character, TrieNode>();
-        this.isLeaf = false;
+    TrieNode(char v, boolean b) {
+        this.val = v;
+        this.isLeaf = b;
+        this.dict = new HashMap<>();
     }
 }
 
-public class Trie {
-    private TrieNode root;
+class Trie {
+    TrieNode root;
 
+    /** Initialize your data structure here. */
     public Trie() {
-        root = new TrieNode('\0');
+        this.root = new TrieNode('\0', true);
     }
-
-    // Inserts a word into the trie.
+    
+    /** Inserts a word into the trie. */
     public void insert(String word) {
         TrieNode curr = root;
         for (int i = 0; i < word.length(); i++) {
             char currChar = word.charAt(i);
-            if (curr.dict.containsKey(currChar) == false) {
-                TrieNode next = new TrieNode(currChar);
-                curr.dict.put(currChar, next);
-                curr = next;
-            } else {
-                TrieNode next = curr.dict.get(currChar);
-                curr = next;
+            if (!curr.dict.containsKey(currChar)) {
+                curr.dict.put(currChar, new TrieNode(currChar, false));
             }
             
+            curr = curr.dict.get(currChar);
             if (i == word.length() - 1) {
                 curr.isLeaf = true;
             }
         }
     }
-
-    // Returns if the word is in the trie.
+    
+    /** Returns if the word is in the trie. */
     public boolean search(String word) {
         TrieNode curr = root;
         for (int i = 0; i < word.length(); i++) {
             char currChar = word.charAt(i);
-            if (curr.dict.containsKey(currChar) == false) {
+            if (!curr.dict.containsKey(currChar)) {
                 return false;
-            } else {
-                curr = curr.dict.get(currChar);
             }
+            
+            curr = curr.dict.get(currChar);
         }
         
         return curr.isLeaf;
     }
-
-    // Returns if there is any word in the trie
-    // that starts with the given prefix.
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
         TrieNode curr = root;
         for (int i = 0; i < prefix.length(); i++) {
             char currChar = prefix.charAt(i);
-            if (curr.dict.containsKey(currChar) == false) {
+            if (!curr.dict.containsKey(currChar)) {
                 return false;
-            } else {
-                curr = curr.dict.get(currChar);
             }
+            
+            curr = curr.dict.get(currChar);
         }
         
         return true;
     }
 }
 
-// Your Trie object will be instantiated and called as such:
-// Trie trie = new Trie();
-// trie.insert("somestring");
-// trie.search("key");
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
