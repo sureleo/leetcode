549aabb4d66f895901fb92ba0613dc8d6e136940
Shuo LI <surezeroleo@gmail.com>
2019-10-14 21:47:23 -0700

    Update SerializeAndDeserializeBinaryTree.java

--- java/SerializeAndDeserializeBinaryTree.java
+++ java/SerializeAndDeserializeBinaryTree.java
@@ -8,43 +8,61 @@
  * }
  */
 public class Codec {
-    List<String> list = new ArrayList<String>();
-    public void mySerialize(TreeNode root) {
+    private void mySerialize(TreeNode root, List<String> list) {
         if (root == null) {
             list.add("#");
             return;
         }
 
         list.add(Integer.toString(root.val));
-        mySerialize(root.left);
-        mySerialize(root.right);
+        mySerialize(root.left, list);
+        mySerialize(root.right, list);
     }
 
-    public TreeNode myDeserialize() {
+    private TreeNode myDeserialize(LinkedList<String> list) {
         if (list.size() == 0 || list.get(0).equals("#")) {
             if (list.size() != 0) {
-                list = list.subList(1, list.size());
+                list.remove(0);
             }
 
             return null;
         }
 
         TreeNode root = new TreeNode(Integer.parseInt(list.get(0)));
-        list = list.subList(1, list.size());
-        root.left = myDeserialize();
-        root.right = myDeserialize();
+        list.remove(0);
+        root.left = myDeserialize(list);
+        root.right = myDeserialize(list);
         return root;
     }
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        mySerialize(root);
-        return list.toString();
+        List<String> list = new ArrayList<String>();
+        mySerialize(root, list);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+            
+            if (i != list.size() - 1) {
+                sb.append(",");
+            }
+        }
+        
+        return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        return myDeserialize();
+        String[] stringArray = data.split(",");
+        LinkedList<String> stringList = new LinkedList<>();
+        
+        for (int i = 0; i < stringArray.length; i++) {
+            stringList.add(stringArray[i]);
+        }
+        
+        return myDeserialize(stringList);
     }
 }
 
